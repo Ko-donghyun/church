91e6bfa8dc3bc2bb59df137e529c49600241dcf4
kodong <kodonghyun1209@gmail.com>
2016-09-23 15:04:03 +0900

    유저 모델 추가
    sequelize 설정을 바탕으로 유저의 모델 추가하고 난 후 유저 테이블 생성하는 파일 추가

---
+++ model/user.js
@@ -0,0 +1,37 @@
+
+var winston = require('winston');
+var Sequelize = require('sequelize');
+
+var sequelize = require('./../config/env/sequelize.js');
+
+var User = sequelize.define('user', {
+  uuid: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true
+  },
+  gender: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  age: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  churchName:  {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+}, {
+  timestamps: true,
+  paranoid: true,
+  underscored: false,
+  collate: 'utf8_unicode_ci',
+  engine: 'InnoDB'
+});
+
+User.sync().then(function () {
+  winston.debug('User 디비 생성 완료');
+});
+
+module.exports = User;
