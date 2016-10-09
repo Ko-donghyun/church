7a8897368668a32fe123fb6018fe822618814b3d
kodong <kodonghyun1209@gmail.com>
2016-10-10 08:59:45 +0900

    어플리 케이션 버전 체크 메서드
    어플의 업데이트를 대비한 어플 버전 체크 메서드 추가

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
+
+var currentAppVersion = 1.0;
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/application/check', function(req, res, next) {
+  var appVersion = req.query.appVersion || 0;
+
+  if (appVersion < currentAppVersion) {
+    return res.json({
+      success: 0,
+      message: '버전이 낮습니다.'
+    });
+  }
+
+  res.json({
+    success: 1,
+    message: '최신 버전 입니다.'
+  });
 });
 
 module.exports = router;
