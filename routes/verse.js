478b8112d20084a9411c4ab6ce04c5f74b6a4beb
kodong <kodonghyun1209@gmail.com>
2016-10-16 10:02:49 +0900

    `랜덤으로 성경 리스트 불러오기 컨트롤러` 수정
    좋아요 추가에 따른 쿼리 변경

--- routes/verse.js
+++ routes/verse.js
@@ -100,14 +100,10 @@ router.post('/bible', function(req, res, next) {
 router.get('/randomList', function(req, res, next) {
   winston.debug('랜덤으로 성경 리스트 불러오기 컨트롤러 시작');
 
-  var limit = 20;
+  var userId = req.query.userId;
+  var query = "SELECT v.*, l.id AS isLike FROM verses AS v LEFT OUTER JOIN likes AS l ON v.id = l.verseId AND l.userId = " + userId + " ORDER BY RAND() LIMIT 20;";
 
-  Verse.findAll({
-    order: [
-      Sequelize.fn( 'RAND' )
-    ],
-    limit: limit
-  }).then(function(result) {
+  sequelize.query(query, {type: sequelize.QueryTypes.SELECT}).then(function(result) {
     winston.debug('랜덤으로 성경 리스트 불러오기 완료');
     
     res.json({
